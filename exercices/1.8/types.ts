57f0c0ab5c95efb8da8c1ef68266872422e41bfe
Dawid Sac <dawidsac24@gmail.com>
2025-10-10 14:09:27 +0200

    new: ex 1.8

---
+++ exercices/1.8/types.ts
@@ -0,0 +1,35 @@
+interface Pizza {
+  id: number;
+  title: string;
+  content: string;
+}
+
+interface PizzaToUpdate {
+  title?: string;
+  content?: string;
+}
+
+type NewPizza = Omit<Pizza, "id">;
+
+export type { Pizza, NewPizza, PizzaToUpdate };
+
+interface Movie {
+  id: number;
+  title: string;
+  director: string;
+  duration: number;
+  budget?: number;
+  description?: string;
+  imageUrl?: string;
+}
+
+interface NewMovie {
+  title: string;
+  director: string;
+  duration: number;
+  budget?: number;
+  description?: string;
+  imageUrl?: string;
+}
+
+export type { Movie, NewMovie };
